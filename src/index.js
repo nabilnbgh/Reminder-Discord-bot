bcf63616dbdf14b6d840d9de6c48394f07473d28
Uyamikun <nabil.nabighah@gmail.com>
2022-01-24 23:29:19 +0700

    add notification bot

--- src/index.js
+++ src/index.js
@@ -6,5 +6,16 @@ const client = new Discord.Client({
         Discord.Intents.FLAGS.GUILD_MESSAGES
     ]
 });
+
+client.on('ready',() => {
+    console.log(min);
+    var sendChannel = client.channels.cache.find(channel => channel.id === '935200576273264650')
+    sendChannel.send("<@330683045684445190> Login Hoyolab");
+
+    
+        
+    
+
+});
 client.login(process.env.DISCORD_TOKEN);
 
