403855d24196aa7c926e8ff96546ac9d79ccfe8f
Uyamikun <63537058+Uyamikun@users.noreply.github.com>
2022-01-24 19:50:11 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const Discord = require("discord.js");
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+client.login(process.env.DISCORD_TOKEN);
+
