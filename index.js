db3aa961df87496e54f9ba9a5a7a3d49a6676dda
Uyamikun <nabil.nabighah@gmail.com>
2022-01-25 00:34:16 +0700

    fix error

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ client.on('ready',() => {
     var sendChannel = client.channels.cache.find(channel => channel.id === '935200576273264650')
     var seconds = date.getSeconds();
     console.log(seconds);
-    if(seconds === 10 || seconds === 10 || seconds || 30){
-        sendChannel.send("<@330683045684445190> Login Hoyolab");
-    }
-    
+    sendChannel.send("<@330683045684445190> Login Hoyolab");
     
 });
 client.login(process.env.DISCORD_TOKEN);
