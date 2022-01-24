8f5b745d1f45c6a927425fdd65adb16977da651e
Uyamikun <nabil.nabighah@gmail.com>
2022-01-25 00:22:21 +0700

    cek interval

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ const client = new Discord.Client({
 });
 
 client.on('ready',() => {
+    let date = new Date();
     console.log("client is ready");
     var sendChannel = client.channels.cache.find(channel => channel.id === '935200576273264650')
-    sendChannel.send("<@330683045684445190> Login Hoyolab");
+    var seconds = date.getSeconds();
+    while(true){
+        if(seconds === 10 || seconds === 10 || seconds || 30){
+            sendChannel.send("<@330683045684445190> Login Hoyolab");
+        };
+    };
+    
 });
 client.login(process.env.DISCORD_TOKEN);
 
