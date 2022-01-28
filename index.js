615c876ad403a4aa4537bb77c4287b5fd8d2f9dc
Uyamikun <nabil.nabighah@gmail.com>
2022-01-28 13:39:57 +0700

    update notif

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.on('ready',() => {
     var sendChannel = client.channels.cache.find(channel => channel.id === '935200576273264650')
     var seconds = date.getSeconds();
     console.log(seconds);
-    sendChannel.send("<@330683045684445190> Login Hoyolab");
+    sendChannel.send("<@330683045684445190> Login Hoyolab sama event web");
     
 });
 client.login(process.env.DISCORD_TOKEN);
