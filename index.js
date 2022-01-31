c19cf28ede52f58465e468f26010404e92231a4c
Uyamikun <nabil.nabighah@gmail.com>
2022-01-31 22:20:33 +0700

    listtask

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 const Discord = require("discord.js");
+const { MessageEmbed } = require('discord.js');
+const fs = require('fs');
+const { off } = require('process');
+const prefix = "$";
+// setting up discord bot
 const client = new Discord.Client({
     intents: [
         Discord.Intents.FLAGS.GUILDS,
@@ -7,15 +12,65 @@ const client = new Discord.Client({
     ]
 });
 
+
+
+
 client.on('ready',() => {
     let date = new Date();
     console.log("client is ready");
     var sendChannel = client.channels.cache.find(channel => channel.id === '935200576273264650')
     var seconds = date.getSeconds();
-    console.log(seconds);
     sendChannel.send("<@330683045684445190> Login Hoyolab event web");
-    sendChannel.send("https://mhy.link/05ehQGA6");
+    // sendChannel.send("https://mhy.link/05ehQGA6");
     
 });
+
+client.on('messageCreate',(message) => {
+    if(message.content.startsWith(prefix)){
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        if(command === "addtask"){
+            message.channel.send('Pongcrazy');
+        }
+        else if(command === "listtask"){
+            fs.readFile('./tugas.json','utf-8',(err,jsonString) =>{
+                if(err){
+                    console.log(err)
+                }
+                else{
+                    //catch error from parse if exist
+                    try{
+                        const parseData = JSON.parse(jsonString);
+                        parseData.listTugas.forEach(tugas => {
+                            const exampleEmbed = new MessageEmbed()
+                            .setColor('#0099ff')
+                            .setTitle(tugas.matkul)      
+                            .setDescription(tugas.judul)
+                            .addFields(
+                                { name: "Deadline", value: tugas.deadline },
+                            );
+                            message.channel.send({ embeds: [exampleEmbed] });
+                        });
+                    }catch(err){
+                        console.log("Error parsing JSON",err);
+                    }
+            
+                }
+            });
+
+        }
+        else{
+            message.channel.send("That's not a right command");
+        }
+    }
+});
+
+
+
+
+
 client.login(process.env.DISCORD_TOKEN);
+
+
 
