36c9b58df95779c0dadecbaeefb98b585a42b482
Uyamikun <nabil.nabighah@gmail.com>
2022-02-02 14:00:56 +0700

    add procfile

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ client.on('ready',() => {
     var sendChannel = client.channels.cache.find(channel => channel.id === '935200576273264650')
     var hour = date.getHours();
     if(hour === 10){
-        sendChannel.send("<@330683045684445190> Login Hoyolab event web");
-        sendChannel.send("https://mhy.link/05ehQGA6");
+        sendChannel.send("<@330683045684445190> Login Hoyolab");
     }
     
     
